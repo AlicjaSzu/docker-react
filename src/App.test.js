23a988bea2d6536195a31d0c648dbd70372b42a2
AlicjaSzu <alicja.szukuc@gmail.com>
2022-02-06 16:16:25 +0100

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link jyvuj kjb', () => {
+test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
